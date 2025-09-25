7c5c2001945437e76d1d41d2cdb1f31cd44a0182
Saisha Patil <saishap4321@gmail.com>
2025-09-25 11:56:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+class MyRunnable implements Runnable
+{
+public void run()
+{
+System.out.println("Thread is running....");
+}
+}
+
+public class Main
+{
+public static void main(String args[])
+{
+MyRunnable runnable=new MyRunnable();
+Thread t1=new Thread(runnable);
+t1.start();
+}
+}
